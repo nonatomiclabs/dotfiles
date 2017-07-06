2ed23324089576b4b70f5d35b4c1e067cf903923
Jean Cruypenynck <jcruypenynck@flux.to>
2017-07-06 12:49:19 +0200

    Add binding of numeric keypad

--- awesomeTerminal.sh
+++ awesomeTerminal.sh
@@ -28,3 +28,6 @@ rm -rf ~/Desktop/fonts
 
 # Change the default shell to zsh
 chsh -s /bin/zsh
+
+# Bind keypad keys
+echo '\n\n# Keypad\n# 0 . Enter\nbindkey -s "^[Op" "0"\nbindkey -s "^[Ol" "."\nbindkey -s "^[OM" "^M"\n# 1 2 3\nbindkey -s "^[Oq" "1"\nbindkey -s "^[Or" "2"\nbindkey -s "^[Os" "3"\n# 4 5 6\nbindkey -s "^[Ot" "4"\nbindkey -s "^[Ou" "5"\nbindkey -s "^[Ov" "6"\n# 7 8 9\nbindkey -s "^[Ow" "7"\nbindkey -s "^[Ox" "8"\nbindkey -s "^[Oy" "9"\n# + -  * /\nbindkey -s "^[Ok" "+"\nbindkey -s "^[Om" "-"\nbindkey -s "^[Oj" "*"\nbindkey -s "^[Oo" "/"' >> ~/.zshrc
